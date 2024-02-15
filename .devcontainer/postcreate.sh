2b7a9c9f908dd0823388f80acc2a27821d3eb3c0
Dennis Shendrik <mikatuo@users.noreply.github.com>
2024-02-15 09:49:42 -0700

    fix errors on new Macs
    by forcing devcontainer to run on linux/amd64

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,21 +1,21 @@
+# install python3
 sudo apt update
 sudo apt install -y python3-pip
 
-# requirements
+# install libs for update.py
 pip3 install --user lunr==0.5.5
 pip3 install --user requests
 pip3 install --user pyyaml
 pip3 install --user markdown==2.6.9
 pip3 install --user pygments==2.1.3
 
-# update the site with updated content from external sources/repositories
+# fix error "libmodelc_shared.so: libXext.so.6: cannot open shared object file: No such file or directory"
+sudo apt install -y libxtst6 libxt6
+sudo ln -s /usr/local/lib/R/lib/libR.so /lib/x86_64-linux-gnu/libR.so
+
+# download content from other repositories
 python3 update.py --download docs refdoc
+python3 update.py --download asset-portal games-showcase
 
-# python2 update.py --download codepad examples assets
-
-# the above command fails because of the error:
-
-# Working...Reading classes...Unable to load library 'SpineExt':
-# libSpineExt.so: cannot open shared object file: No such file or directory
-# libSpineExt.so: cannot open shared object file: No such file or directory
-# Native library (linux-aarch64/libSpineExt.so) not found in resource path ([file:/tmp/tmpn2ly7mln/examples-master/build/plugins/defold-spine/plugins/share/pluginSpineExt.jar])
+# python3 update.py --download codepad
+# python3 update.py --download examples
